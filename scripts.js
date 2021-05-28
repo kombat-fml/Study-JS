cb338802038cf6f0745de9e2a92e577fe3c88af8
Anton Shakhov <informativpeople@gmail.com>
2021-05-28 15:35:37 +0300

    Усложненное задание 2 урок

--- scripts.js
+++ scripts.js
@@ -1,18 +1,8 @@
-let money = 30000,
-  income = 'фриланс, консультации, кураторство',
-  addExpenses = 'телефон, интернет, коммуналка, кино',
-  deposit = true,
-  mission = 50000,
-  period = 2;
+const num = 266219;
+let product = 1;
+for (let i = 0; i < num.toString().length; i++) {
+  product *= num.toString()[i];
+}
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log(`Цель заработать ${mission} рублей`);
-
-const expenseElems = addExpenses.toLowerCase().split(', ');
-console.log(expenseElems);
-
-const budgetDay = money / 30;
-console.log(budgetDay);
+let res = product ** 3;
+console.log(res.toString().substr(0, 2));
