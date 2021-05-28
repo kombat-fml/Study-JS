ec15ecf7240e56a32d6a8874a4d9a61da5f0adf2
Anton Shakhov <informativpeople@gmail.com>
2021-05-28 15:22:12 +0300

    Lesson-2 completed

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
-let money, income, addExpenses, deposit, mission, period;
+let money = 30000,
+  income = 'фриланс, консультации, кураторство',
+  addExpenses = 'телефон, интернет, коммуналка, кино',
+  deposit = true,
+  mission = 50000,
+  period = 2;
 
-alert('Hello, World!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Цель заработать ${mission} рублей`);
 
-console.log('Привет, Саша! Где ключ от танка?');
+const expenseElems = addExpenses.toLowerCase().split(', ');
+console.log(expenseElems);
+
+const budgetDay = money / 30;
+console.log(budgetDay);
