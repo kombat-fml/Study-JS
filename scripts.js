fd0946d99a78d7a4ea5731270889c2ce1a6f4223
Anton Shakhov <informativpeople@gmail.com>
2021-05-31 11:56:51 +0300

    complete lesson3 hard

--- scripts.js
+++ scripts.js
@@ -1,8 +1,97 @@
-const num = 266219;
-let product = 1;
-for (let i = 0; i < num.toString().length; i++) {
-  product *= num.toString()[i];
+const lang = 'ru';
+const daysRu = [
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+  'Воскресенье',
+];
+const daysEng = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
+const days = [
+  'ru',
+  [
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота',
+    'Воскресенье',
+  ],
+  'en',
+  [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ],
+];
+const daysObj = {
+  ru: [
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота',
+    'Воскресенье',
+  ],
+  en: [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ],
+};
+
+// вариант через If
+if (lang === 'ru') {
+  console.log(daysRu);
+} else {
+  console.log(daysEng);
 }
 
-let res = product ** 3;
-console.log(res.toString().substr(0, 2));
+// варинат через switch-case
+switch (lang) {
+  case 'ru':
+    console.log(daysRu);
+    break;
+  case 'en':
+    console.log(daysEng);
+    break;
+  default:
+    console.log('Неизвестное значение lang');
+}
+
+//вариант через многомерный массив
+console.log(days[days.indexOf(lang) + 1]);
+
+// вариант через объект
+console.log(daysObj[lang]);
+
+// Задание №2
+const namePerson = 'Артемий';
+
+console.log(
+  namePerson === 'Артем'
+    ? 'директор'
+    : namePerson === 'Максим'
+    ? 'преподаватель'
+    : 'студент'
+);
