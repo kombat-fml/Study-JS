d995f133d95d7cc749ac69e1a5bb3e52c47ccc5f
Anton Shakhov <informativpeople@gmail.com>
2021-06-03 12:21:07 +0300

    complete lesson 6 hard

--- scripts.js
+++ scripts.js
@@ -1,57 +1,38 @@
 'use strict';
+const isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
 
-const arr = ['12333', '2664364', '3332', '44332', '5454', '67373', '799320'];
-let primeArr = [];
+const startGame = function () {
+  const currentNumber = Math.ceil(Math.random() * 100);
+  console.log(currentNumber);
+  let countAttempts = 10;
 
-// функция нахождения чисел, начинающихся с 2 или 4
-const filterArray = function (arr, callback) {
-  const outputArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i][0] === '2' || arr[i][0] === '4') {
-      outputArr.push(arr[i]);
+  function askUser(msg) {
+    let userString = prompt(msg);
+    if (countAttempts <= 1) {
+      if (confirm('Попытки закончились, хотите сыграть еще?')) {
+        return startGame();
+      } else {
+        return 'Спасибо за игру!';
+      }
+    } else if (userString === null) {
+      return 'Игра окончена';
+    } else if (userString === '' || !isNumber(userString)) {
+      return askUser('Введи число!');
+    } else if (+userString > currentNumber) {
+      countAttempts--;
+      return askUser(`Загаданное число меньше, осталось попыток ${countAttempts}`);
+    } else if (+userString < currentNumber) {
+      countAttempts--;
+      return askUser(`Загаданное число больше, осталось попыток ${countAttempts}`);
+    } else if (confirm('Поздравляю, Вы угадали!!!! Хотели бы сыграть еще?')) {
+      return startGame();
+    } else {
+      return 'Спасибо за игру!';
     }
   }
-  callback(outputArr);
+  return askUser('Угадай число от 1 до 100');
 };
 
-// функция формирования массива чисел от 1 до n
-const collectArray = function (n) {
-  const arr = [];
-  for (let i = 0; i < n; i++) {
-    arr.push(i + 1);
-  }
-  return arr;
-};
-
-// функция "исключения" составных чисел из массива
-const removeСompositeNumbers = function (arr, number) {
-  for (let i = 1; i <= Math.floor(arr.length / number); i++) {
-    arr[i * number - 1] = 1;
-  }
-  return arr;
-};
-
-// функция поиска простых чисел
-const findPrimeNumbers = function (arr, callback) {
-  const outputArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= 2) {
-      outputArr.push(arr[i]);
-      removeСompositeNumbers(arr, i + 1);
-    }
-  }
-  callback(outputArr);
-};
-
-filterArray(arr, function (arr) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
-});
-
-primeArr = collectArray(100);
-findPrimeNumbers(primeArr, function (arr) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(`Простое число ${arr[i]}. Делители этого числа 1 и ${arr[i]}`);
-  }
-});
+alert(startGame());
