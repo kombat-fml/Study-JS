349cd8c96d55f7877e01f0f52ba114212b2e1909
Anton Shakhov <informativpeople@gmail.com>
2021-06-04 15:59:10 +0300

    complete lesson 07 hard level

--- scripts.js
+++ scripts.js
@@ -1,97 +1,15 @@
-const lang = 'ru';
-const daysRu = [
-  'Понедельник',
-  'Вторник',
-  'Среда',
-  'Четверг',
-  'Пятница',
-  'Суббота',
-  'Воскресенье',
-];
-const daysEng = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday',
-];
-const days = [
-  'ru',
-  [
-    'Понедельник',
-    'Вторник',
-    'Среда',
-    'Четверг',
-    'Пятница',
-    'Суббота',
-    'Воскресенье',
-  ],
-  'en',
-  [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday',
-  ],
-];
-const daysObj = {
-  ru: [
-    'Понедельник',
-    'Вторник',
-    'Среда',
-    'Четверг',
-    'Пятница',
-    'Суббота',
-    'Воскресенье',
-  ],
-  en: [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday',
-  ],
-};
+'use strict';
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  date = new Date();
 
-// вариант через If
-if (lang === 'ru') {
-  console.log(daysRu);
-} else {
-  console.log(daysEng);
-}
-
-// варинат через switch-case
-switch (lang) {
-  case 'ru':
-    console.log(daysRu);
-    break;
-  case 'en':
-    console.log(daysEng);
-    break;
-  default:
-    console.log('Неизвестное значение lang');
-}
-
-//вариант через многомерный массив
-console.log(days[days.indexOf(lang) + 1]);
-
-// вариант через объект
-console.log(daysObj[lang]);
-
-// Задание №2
-const namePerson = 'Артемий';
-
-console.log(
-  namePerson === 'Артем'
-    ? 'директор'
-    : namePerson === 'Максим'
-    ? 'преподаватель'
-    : 'студент'
-);
+week.forEach(function (item, i) {
+  if (i !== date.getDay() - 1) {
+    if (i === 5 || i === 6) {
+      document.body.insertAdjacentHTML('beforeend', '<p><i>' + item + '<i></p>');
+    } else {
+      document.body.insertAdjacentHTML('beforeend', '<p>' + item + '</p>');
+    }
+  } else {
+    document.body.insertAdjacentHTML('beforeend', '<p><b>' + item + '</b></p>');
+  }
+});
