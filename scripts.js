13c21e921217d41c6f2240c1e86f5274fe646b65
Anton Shakhov <informativpeople@gmail.com>
2021-06-07 21:33:47 +0300

    fix querySelectors lesson 09

--- scripts.js
+++ scripts.js
@@ -6,15 +6,21 @@ const startBtn = document.getElementById('start'),
   expensesAdd = document.getElementsByTagName('button')[1],
   depositCheck = document.querySelector('#deposit-check'),
   additionalIncomeItems = document.querySelectorAll('.additional_income-item'),
-  outputElems = document.querySelectorAll('[class$="-value"]'),
   salaryAmount = document.querySelector('.salary-amount'),
-  incomeTitle = document.querySelector('.income-title'),
+  incomeTitle = document.querySelector('input.income-title'),
   incomeAmount = document.querySelector('.income-amount'),
-  expensesTitle = document.querySelector('.expenses-title'),
+  expensesTitle = document.querySelector('input.expenses-title'),
   expensesAmount = document.querySelector('.expenses-amount'),
   additionalExpensesItem = document.querySelector('.additional_expenses-item'),
   targetAmount = document.querySelector('.target-amount'),
-  periodSelect = document.querySelector('.period-select');
+  periodSelect = document.querySelector('.period-select'),
+  budgetDayValue = document.querySelector('.budget_day-value'),
+  budgetMonthValue = document.querySelector('.budget_month-value'),
+  additionalIncomeValue = document.querySelector('.additional_income-value'),
+  additionalExpensesValue = document.querySelector('.additional_expenses-value'),
+  incomePeriodValue = document.querySelector('.income_period-value'),
+  targetMonthValue = document.querySelector('.target_month-value'),
+  expensesMonthValue = document.querySelector('.expenses_month-value');
 
 function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
