18b35f00a0f3d7247065e6f57bea31c54f96540c
Anton Shakhov <informativpeople@gmail.com>
2021-06-07 17:33:27 +0300

    complete lesson 9 hard level

--- scripts.js
+++ scripts.js
@@ -1,14 +1,60 @@
 'use strict';
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  date = new Date();
-let htmlString;
-week.forEach(function (item, i) {
-  htmlString = item;
-  if (i === date.getDay() - 1) {
-    htmlString = '<b>' + htmlString + '</b>';
+
+const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+  month = [
+    'Января',
+    'Февраля',
+    'Марта',
+    'Апреля',
+    'Мая',
+    'Июня',
+    'Июля',
+    'Августа',
+    'Сентября',
+    'Октября',
+    'Ноября',
+    'Декабря',
+  ],
+  time1 = document.querySelector('.time1'),
+  time2 = document.querySelector('.time2');
+
+const endingWord = function (n, gender) {
+  let string;
+  switch (true) {
+    case n <= 14 && n >= 11:
+      string = gender === 'hours' ? 'ов' : '';
+      break;
+    case n % 10 === 1:
+      string = gender === 'hours' ? '' : 'а';
+      break;
+    case n % 10 >= 2 && n % 10 <= 4:
+      string = gender === 'hours' ? 'а' : 'ы';
+      break;
+    default:
+      string = gender === 'hours' ? 'ов' : '';
   }
-  if (i === 5 || i === 6) {
-    htmlString = '<i>' + htmlString + '</i>';
-  }
-  document.body.insertAdjacentHTML('beforeend', `<p>${htmlString}</p>`);
-});
+  return string;
+};
+const formatNumber = function (n) {
+  return Math.floor(n / 10) === 0 ? '0' + n : n;
+};
+
+setInterval(function () {
+  const date = new Date();
+  let htmlString1, htmlString2;
+  htmlString1 = `Сегодня ${week[date.getDay()]}, ${date.getDate()} ${
+    month[date.getMonth()]
+  }, ${date.getHours()} час${endingWord(date.getHours(), 'hours')} ${date.getMinutes()} минут${endingWord(
+    date.getMinutes(),
+    'min'
+  )} ${date.getSeconds()} секунд${endingWord(date.getSeconds(), 'seconds')}`;
+
+  htmlString2 = `${formatNumber(date.getDate())}.${formatNumber(
+    date.getMonth()
+  )}.${date.getFullYear()} - ${formatNumber(date.getHours())}:${formatNumber(date.getMinutes())}:${formatNumber(
+    date.getSeconds()
+  )}
+  `;
+  time1.textContent = htmlString1;
+  time2.textContent = htmlString2;
+}, 1000);
