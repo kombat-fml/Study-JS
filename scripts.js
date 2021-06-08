28227922e3c23704f0134445bd5af88153254364
Anton Shakhov <informativpeople@gmail.com>
2021-06-08 11:49:06 +0300

    fix lesson 10

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@
 
 const adv = document.querySelector('.adv'),
   books = document.querySelectorAll('.book'),
-  list = books[5].querySelectorAll('li'),
+  listBook2 = books[0].querySelectorAll('li'),
+  listBook5 = books[5].querySelectorAll('li'),
   listBook6 = books[2].querySelectorAll('li'),
   newElem = document.createElement('li');
 
@@ -12,8 +13,12 @@ books[4].insertAdjacentElement('afterend', books[3]);
 document.body.style.backgroundImage = 'url(/image/you-dont-know-js.jpg)';
 books[4].querySelector('h2>a').textContent = 'Книга 3. this и Прототипы Объектов';
 adv.remove();
-list[1].after(list[9]);
-list[4].after(list[2]);
-list[7].after(list[5]);
+listBook2[9].after(listBook2[2]);
+listBook2[3].after(listBook2[8]);
+listBook2[3].after(listBook2[6]);
+listBook5[1].after(listBook5[9]);
+listBook5[4].after(listBook5[2]);
+listBook5[7].after(listBook5[5]);
 newElem.textContent = 'Глава 8: За пределами ES6';
 listBook6[listBook6.length - 1].before(newElem);
+console.log(listBook2);
