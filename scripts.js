8f127a828dce2034e0526d9764713afa80e3b070
Anton Shakhov <informativpeople@gmail.com>
2021-06-12 08:06:05 +0300

    complete lesson 14 hard

--- scripts.js
+++ scripts.js
@@ -1,30 +1,49 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict';
 
-function DomElement() {
-  this.selector = '';
-  this.height = '200px';
-  this.width = '300px';
-  this.bg = 'red';
-  this.fontSize = '16px';
-}
-
-DomElement.prototype.createElement = function (element) {
-  let elem;
-  if (element[0] === '.') {
-    elem = document.createElement('div');
-    elem.className = element.substr(1);
-  } else if (element[0] === '#') {
-    elem = document.createElement('p');
-    elem.id = element.substr(1);
+  function DomElement() {
+    this.selector = '';
+    this.height = '100px';
+    this.width = '100px';
+    this.bg = 'red';
+    this.position = 'absolute';
   }
-  elem.style.height = this.height;
-  elem.style.width = this.width;
-  elem.style.background = this.bg;
-  elem.style.fontSize = this.fontSize;
 
-  elem.textContent = 'Какой-то текст';
-  document.body.append(elem);
-};
+  DomElement.prototype.createElement = function () {
+    let elem;
+    elem = document.createElement('div');
+    elem.style.height = this.height;
+    elem.style.width = this.width;
+    elem.style.background = this.bg;
+    elem.style.fontSize = this.fontSize;
+    elem.style.position = this.position;
+    elem.style.left = '0';
+    elem.style.top = '0';
+    document.body.append(elem);
+    return elem;
+  };
 
-let div = new DomElement();
-div.createElement('#fdgfd');
+  let div = new DomElement();
+  const divElement = div.createElement();
+
+  document.body.addEventListener('keydown', function (event) {
+    switch (true) {
+      case event.code === 'ArrowRight': {
+        divElement.style.left = parseInt(divElement.style.left) + 10 + 'px';
+        break;
+      }
+      case event.code === 'ArrowLeft': {
+        divElement.style.left = parseInt(divElement.style.left) - 10 + 'px';
+        break;
+      }
+      case event.code === 'ArrowUp': {
+        divElement.style.top = parseInt(divElement.style.top) - 10 + 'px';
+        break;
+      }
+      case event.code === 'ArrowDown': {
+        divElement.style.top = parseInt(divElement.style.top) + 10 + 'px';
+        break;
+      }
+    }
+  });
+});
