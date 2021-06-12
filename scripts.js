a8132caadb2c1b4d9e2de279eedcdd9cf8e4488a
Anton Shakhov <informativpeople@gmail.com>
2021-06-12 20:40:19 +0300

    start project with registration and authorization

--- scripts.js
+++ scripts.js
@@ -1,49 +1,117 @@
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
+  const span = document.getElementById('username'),
+    registrationBtn = document.getElementById('registration'),
+    loginBtn = document.getElementById('login'),
+    list = document.getElementById('list'),
+    month = [
+      'Января',
+      'Февраля',
+      'Марта',
+      'Апреля',
+      'Мая',
+      'Июня',
+      'Июля',
+      'Августа',
+      'Сентября',
+      'Октября',
+      'Ноября',
+      'Декабря',
+    ];
 
-  function DomElement() {
-    this.selector = '';
-    this.height = '100px';
-    this.width = '100px';
-    this.bg = 'red';
-    this.position = 'absolute';
-  }
+  let usersData;
 
-  DomElement.prototype.createElement = function () {
-    let elem;
-    elem = document.createElement('div');
-    elem.style.height = this.height;
-    elem.style.width = this.width;
-    elem.style.background = this.bg;
-    elem.style.fontSize = this.fontSize;
-    elem.style.position = this.position;
-    elem.style.left = '0';
-    elem.style.top = '0';
-    document.body.append(elem);
-    return elem;
+  const findUser = function (place, value) {
+    let flag = false;
+    usersData.forEach(function (item, i) {
+      switch (true) {
+        case place === 'user': {
+          if (value === item.name + item.surname) {
+            flag = true;
+            return true;
+          }
+        }
+      }
+    });
   };
 
-  let div = new DomElement();
-  const divElement = div.createElement();
+  const readFromLocalStorage = function () {
+    let json = localStorage.getItem('UsersList');
+    json = JSON.parse(json);
+    return json ? json : [];
+  };
 
-  document.body.addEventListener('keydown', function (event) {
+  const writeToLocalStorage = function (data) {
+    const json = JSON.stringify(data);
+    localStorage.setItem('UsersList', json);
+  };
+
+  const formatNumber = function (n) {
+    return Math.floor(n / 10) === 0 ? '0' + n : n;
+  };
+
+  const render = function () {
+    list.textContent = '';
+    usersData.forEach(function (item, i) {
+      const li = document.createElement('li');
+      li.innerHTML =
+        '<span>' +
+        'Имя: ' +
+        item.name +
+        ', фамилия: ' +
+        item.surname +
+        ', зарегистрирован: ' +
+        item.dateOfReg +
+        '</span><button class="remove-user"></button>';
+      list.append(li);
+
+      li.querySelector('.remove-user').addEventListener('click', function () {
+        li.remove();
+        usersData.splice(i, 1);
+        render();
+      });
+    });
+    writeToLocalStorage(usersData);
+    console.log(usersData);
+  };
+
+  const registerUser = function () {
+    const user = prompt('Введите имя и фамилию пользователя через пробел').trim();
     switch (true) {
-      case event.code === 'ArrowRight': {
-        divElement.style.left = parseInt(divElement.style.left) + 10 + 'px';
+      case user.match(/ /g) === null:
+        if (confirm('Ошибка! В данных введено одно слово, хотите ввести снова?')) {
+          registerUser();
+          return;
+        } else {
+          return;
+        }
         break;
-      }
-      case event.code === 'ArrowLeft': {
-        divElement.style.left = parseInt(divElement.style.left) - 10 + 'px';
+      case user.match(/ /g).length > 1:
+        if (confirm('Ошибка! В данных введено больше одного пробела, хотите ввести снова?')) {
+          registerUser();
+          return;
+        } else {
+          return;
+        }
         break;
-      }
-      case event.code === 'ArrowUp': {
-        divElement.style.top = parseInt(divElement.style.top) - 10 + 'px';
-        break;
-      }
-      case event.code === 'ArrowDown': {
-        divElement.style.top = parseInt(divElement.style.top) + 10 + 'px';
-        break;
-      }
+      default:
     }
-  });
+    const login = prompt('Придумайте логин');
+    const pass = prompt('Придумайте пароль');
+    const date = new Date();
+    const newUser = {
+      name: user.substring(0, user.match(/ /).index),
+      surname: user.substring(user.match(/ /).index + 1),
+      login: login,
+      password: pass,
+      dateOfReg: `${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()} г.,
+        ${formatNumber(date.getHours())}:${formatNumber(date.getMinutes())}:${formatNumber(date.getSeconds())}`,
+    };
+    usersData.push(newUser);
+    render();
+  };
+
+  registrationBtn.addEventListener('click', registerUser);
+  usersData = readFromLocalStorage();
+  render();
 });
