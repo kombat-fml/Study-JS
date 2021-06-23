65eff90dfd603bb42713df7d40078b9c319fc89f
Anton Shakhov <informativpeople@gmail.com>
2021-06-23 21:17:21 +0300

    create regular expressions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -302,4 +302,96 @@ window.addEventListener('DOMContentLoaded', () => {
     startSlider(1500);
   };
   slider();
+
+  // наша команда
+  const command = () => {
+    const commandElements = document.querySelectorAll('.command__photo');
+    const changePhoto = function () {
+      const commonSrc = this.getAttribute('src');
+      this.setAttribute('src', this.dataset.img);
+      this.dataset.img = commonSrc;
+    };
+    commandElements.forEach((elem) => {
+      elem.addEventListener('mouseenter', changePhoto);
+      elem.addEventListener('mouseleave', changePhoto);
+    });
+  };
+  command();
+
+  // калькулятор
+  const calculator = () => {
+    const inputs = document.querySelectorAll('input.calc-item');
+    const replaceLetter = function () {
+      this.value = this.value.replace(/\D/g, '');
+    };
+    inputs.forEach((elem) => {
+      elem.addEventListener('input', replaceLetter);
+    });
+  };
+  calculator();
+
+  // инпуты
+  const inputs = () => {
+    const nameReplacer = function () {
+      this.value = this.value.replace(/[^а-яё -]/gi, '');
+    };
+    const mailReplacer = function () {
+      this.value = this.value.replace(/(?![~@\-!_\.\*'])[\W\d]/gi, '');
+    };
+    const phoneReplacer = function () {
+      this.value = this.value.replace(/(?![\(\)\-])\D/gi, '');
+    };
+    const trimStr = function (elem) {
+      elem.value = elem.value.replace(/^[ \-]*/, '');
+      elem.value = elem.value.replace(/[ \-]*$/, '');
+    };
+
+    const replaceSpaces = function (elem) {
+      elem.value = elem.value.replace(/ +/g, ' ');
+    };
+
+    const replaceHyphen = function (elem) {
+      elem.value = elem.value.replace(/-+/g, '-');
+    };
+    const uppAndLow = function (elem) {
+      elem.value = elem.value.replace(/^(.{1})(.*)/, (match, val1, val2) => val1.toUpperCase() + val2.toLowerCase());
+    };
+
+    const textInputs = document.querySelectorAll('[name="user_name"]'),
+      messageInputs = document.querySelector('[name="user_message"]'),
+      mailInputs = document.querySelectorAll('[name="user_email"]'),
+      phoneInputs = document.querySelectorAll('[name="user_phone"]');
+
+    messageInputs.addEventListener('input', nameReplacer);
+    messageInputs.addEventListener('blur', function () {
+      trimStr(this);
+      replaceSpaces(this);
+      replaceHyphen(this);
+      uppAndLow(this);
+    });
+    textInputs.forEach((elem) => {
+      elem.addEventListener('input', nameReplacer);
+      elem.addEventListener('blur', function () {
+        trimStr(this);
+        replaceSpaces(this);
+        replaceHyphen(this);
+        uppAndLow(this);
+      });
+    });
+    mailInputs.forEach((elem) => {
+      elem.addEventListener('input', mailReplacer);
+      elem.addEventListener('blur', function () {
+        trimStr(this);
+        replaceHyphen(this);
+      });
+    });
+    phoneInputs.forEach((elem) => {
+      elem.addEventListener('input', phoneReplacer);
+      elem.addEventListener('blur', function () {
+        trimStr(this);
+        replaceHyphen(this);
+      });
+    });
+  };
+  inputs();
 });
