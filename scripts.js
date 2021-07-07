7d73fb8357993d41c17cf688521ad9a6268b5f7d
Anton Shakhov <informativpeople@gmail.com>
2021-07-07 17:12:33 +0300

    complete insane bonus01

--- scripts.js
+++ scripts.js
@@ -1,5 +1,166 @@
-let money, income, addExpenses, deposit, mission, period;
+'use strict';
 
-alert('Hello, World!');
+const input = document.getElementById('select-cities'),
+  dropdownLists = document.querySelector('.dropdown-lists'),
+  listDefault = document.querySelector('.dropdown-lists__list--default'),
+  listDefaultCol = listDefault.querySelector('.dropdown-lists__col'),
+  listSelect = document.querySelector('.dropdown-lists__list--select'),
+  listSelectCol = listSelect.querySelector('.dropdown-lists__col'),
+  listAutocomplete = document.querySelector('.dropdown-lists__list--autocomplete'),
+  listAutocompleteCol = listAutocomplete.querySelector('.dropdown-lists__col'),
+  button = document.querySelector('.button'),
+  closeButton = document.querySelector('.close-button'),
+  countries = new Map(),
+  cities = [];
 
-console.log('Привет, Саша! Где ключ от танка?');
+const renderList = (country, list = 'none') => {
+  const currentCountry = countries.get(country),
+    countData = list === 'default' ? 3 : currentCountry.cities.length,
+    currentList = list === 'default' ? listDefaultCol : listSelectCol,
+    block = document.createElement('div');
+
+  block.dataset.country = country;
+  block.className = 'dropdown-lists__countryBlock';
+  block.innerHTML = `
+    <div class="dropdown-lists__total-line">
+      <div class="dropdown-lists__country">${country}</div>
+      <div class="dropdown-lists__count">${currentCountry.count}</div>
+    </div>
+  `;
+
+  for (let i = 0; i < countData; i++) {
+    const city = currentCountry.cities[i];
+    block.innerHTML += `
+      <div class="dropdown-lists__line">
+        <div class="dropdown-lists__city" data-link="${city.link}">${city.name}</div>
+        <div class="dropdown-lists__count">${city.count}</div>
+      </div>
+    `;
+  }
+  currentList.appendChild(block);
+};
+
+const renderAutocompleteList = (cities) => {
+  const block = document.createElement('div');
+  block.className = 'dropdown-lists__countryBlock';
+
+  if (cities.length > 0) {
+    cities.forEach((item) => {
+      block.innerHTML += `
+        <div class="dropdown-lists__line">
+          <div class="dropdown-lists__city" data-link="${item.link}">${item.name}</div>
+          <div class="dropdown-lists__count">${item.count}</div>
+        </div>
+      `;
+    });
+  } else {
+    block.textContent = 'Ничего не найдено';
+  }
+
+  listAutocomplete.appendChild(block);
+};
+
+const getData = (data) => {
+  data['RU'].forEach((item) => {
+    countries.set(item.country, { count: +item.count, cities: item.cities.sort((a, b) => b.count - a.count) });
+    cities.push(...item.cities);
+    renderList(item.country, 'default');
+  });
+};
+
+const request = () => {
+  fetch('db_cities.json')
+    .then((response) => {
+      if (response.status !== 200) {
+        throw new Error('status network not 200!');
+      }
+      return response.json();
+    })
+    .then((result) => getData(result))
+    .catch((error) => console.error('Ошибка запроса: ', error));
+};
+
+const showEl = (el) => {
+  el.classList.add('active');
+};
+
+const hideEl = (el) => {
+  el.classList.remove('active');
+};
+
+const clickToDropdown = (event) => {
+  const target = event.target,
+    country = target.closest('.dropdown-lists__countryBlock').dataset.country;
+  console.log(target);
+  if (target.closest('.dropdown-lists__total-line')) {
+    applyInputValue(target.closest('.dropdown-lists__total-line').children[0]);
+    if (target.closest('.dropdown-lists__list--default')) {
+      listSelectCol.textContent = '';
+      renderList(country);
+      showEl(listSelect);
+      hideEl(listDefault);
+    } else {
+      hideEl(listSelect);
+      showEl(listDefault);
+    }
+  } else if (target.closest('.dropdown-lists__line')) {
+    const cityEl = target.closest('.dropdown-lists__line').children[0];
+    applyInputValue(cityEl);
+    button.href = cityEl.dataset.link;
+    showEl(button);
+  }
+};
+
+const closeButtonHandler = () => {
+  input.value = '';
+  hideEl(listDefault);
+  hideEl(listSelect);
+  hideEl(listAutocomplete);
+  hideEl(closeButton);
+  hideEl(button);
+};
+
+const applyInputValue = (elem) => {
+  input.value = elem.textContent;
+  showEl(closeButton);
+};
+
+const changeInput = function () {
+  const value = this.value.trim();
+  if (value !== '') {
+    const reg = new RegExp(`^${value}`, 'i');
+    const suitableCities = cities.filter((item) => reg.test(item.name));
+    listAutocomplete.textContent = '';
+    renderAutocompleteList(suitableCities);
+    hideEl(listSelect);
+    hideEl(listDefault);
+    showEl(listAutocomplete);
+    showEl(closeButton);
+  } else {
+    hideEl(listSelect);
+    hideEl(closeButton);
+    hideEl(listAutocomplete);
+    showEl(listDefault);
+    hideEl(button);
+  }
+};
+
+const addListeners = () => {
+  input.addEventListener('focus', () => {
+    showEl(dropdownLists);
+    showEl(listDefault);
+  });
+
+  dropdownLists.addEventListener('click', clickToDropdown);
+
+  input.addEventListener('input', changeInput);
+
+  closeButton.addEventListener('click', closeButtonHandler);
+
+  // input.addEventListener('blur', () => {
+  //   hideEl(dropdownLists);
+  // });
+};
+
+request();
+addListeners();
